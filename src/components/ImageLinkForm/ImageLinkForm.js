ac30aab3833606764dd54c58f7aaf7dd2846c663
prakash901 <prakashtiwari901@gmail.com>
2020-08-17 20:47:13 +0545

    edit

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -10,7 +10,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit}) => {
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} placeholder="Insert the link of image to be detectedd" />
+                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} placeholder="Insert the image link here" />
                     <button 
                     className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
                     onClick={onButtonSubmit}
